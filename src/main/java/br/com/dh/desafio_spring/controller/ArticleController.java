9b5201c76e16f3358c7c8f83da4ab9f591fd8a95
thays-gama <thays.gama@mercadolivre.com>
2022-10-05 15:05:12 -0300

    add req04

--- src/main/java/br/com/dh/desafio_spring/controller/ArticleController.java
+++ src/main/java/br/com/dh/desafio_spring/controller/ArticleController.java
@@ -2,13 +2,16 @@ package br.com.dh.desafio_spring.controller;
 
 import br.com.dh.desafio_spring.model.Article;
 import br.com.dh.desafio_spring.service.IArticle;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/articles")
+@RequestMapping("/api/v1/articles")
 public class ArticleController {
     @Autowired
     private IArticle articleService;
@@ -17,5 +20,17 @@ public class ArticleController {
     @ResponseStatus(HttpStatus.CREATED)
     public void saveArticle(@RequestBody Article article){
         articleService.save(article);
+    }
+
+    @GetMapping(params = {"category", "freeShipping"})
+    @ResponseStatus(HttpStatus.OK)
+    public List<Article> findAllByCategoryAndFreeShipping(@RequestParam String category, @RequestParam Boolean freeShipping){
+        return articleService.findAllByCategoryAndFreeShipping(category, freeShipping);
+    }
+
+    @GetMapping(params = {"freeShipping", "prestige"})
+    @ResponseStatus(HttpStatus.OK)
+    public List<Article> findAllByFreeShippingAndPrestige(@RequestParam Boolean freeShipping, @RequestParam String prestige){
+        return articleService.findAllByFreeShippingAndPrestige(freeShipping, prestige);
     }
 }
