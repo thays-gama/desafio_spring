247cff9f2412d2d16f8369c5c0229eed08d65184
SouzaGiovannaMELI <giovanna.salmeida@mercadolivre.com>
2022-10-05 17:30:06 -0300

    fix ticket

--- src/main/java/br/com/dh/desafio_spring/repository/TicketRepo.java
+++ src/main/java/br/com/dh/desafio_spring/repository/TicketRepo.java
@@ -3,10 +3,13 @@ package br.com.dh.desafio_spring.repository;
 import br.com.dh.desafio_spring.model.Article;
 import br.com.dh.desafio_spring.model.ArticleTicket;
 import br.com.dh.desafio_spring.model.Ticket;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.stereotype.Repository;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +21,7 @@ public class TicketRepo {
 
     public Optional<Ticket> saveTicket(List<ArticleTicket> articles){
         ArticleRepo articleRepo = new ArticleRepo();
+        ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
 
         Ticket ticket = new Ticket();
         ticket.setId(getAll().size() + 1);
@@ -29,6 +33,15 @@ public class TicketRepo {
 
             ticket.addArticle(articleCompleto);
         });
+
+        List<Ticket> tickets = new ArrayList<>(getAll());
+        tickets.add(ticket);
+
+        try{
+            writer.writeValue(new File(linkFile), tickets);
+        } catch (Exception ex){
+            System.out.println("Erro ao salvar os dados!");
+        }
 
         return Optional.of(ticket);
     }
