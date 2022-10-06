9b55e1bbff196c6bfd9df6f8266202b30b4c570a
mauroacorreia <mauro.acorreia@mercadolivre.com>
2022-10-06 10:26:16 -0300

    feat: get all clients and get client by state at route /api/v1/clients.

--- src/main/java/br/com/dh/desafio_spring/controller/ClientController.java
+++ src/main/java/br/com/dh/desafio_spring/controller/ClientController.java
@@ -5,7 +5,10 @@ import br.com.dh.desafio_spring.exception.RequiredFieldException;
 import br.com.dh.desafio_spring.model.Client;
 import br.com.dh.desafio_spring.service.IClient;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/clients")
@@ -20,5 +23,17 @@ public class ClientController {
     @ResponseStatus(HttpStatus.CREATED)
     public Client save(@RequestBody Client client) throws RequiredFieldException, EmailAlreadyRegisteredException {
         return clientService.save(client);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<Client>> getAllClients(){
+        return new ResponseEntity<>(clientService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(params = "state")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<Client>> getClientsByState(@RequestParam String state){
+        return new ResponseEntity<>(clientService.getByState(state), HttpStatus.OK);
     }
 }
